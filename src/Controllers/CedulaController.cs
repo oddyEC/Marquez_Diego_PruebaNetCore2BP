51d507cc68c51cd1715c85fca32d9957f3b980fd
Diego Márquez <57378696+oddyEC@users.noreply.github.com>
2022-12-12 17:55:37 -0500

    Adaptado para buscar cedula

--- src/Controllers/CedulaController.cs
+++ src/Controllers/CedulaController.cs
@@ -6,6 +6,15 @@ namespace src.Controllers
     [Route("api/[controller]")]
     public class CedulaController : ControllerBase
     {
+        private static readonly List<Usuario> usuarios = new List<Usuario>(){
+        new Usuario(){Cedula="1724956672",Nombre="Diego Marquez"},
+        new Usuario(){Cedula="1724343449",Nombre="Jose Jose"},
+        new Usuario(){Cedula="1724911325",Nombre="Nath Vill"},
+        new Usuario(){Cedula="1724934222",Nombre="For Form"},
+        new Usuario(){Cedula="1724954654",Nombre="Joe Joe"},
+        new Usuario(){Cedula="1724973434",Nombre="Joe Casx"},
+        new Usuario(){Cedula="1724916565",Nombre="Mike"},
+        };
 
         public readonly ILogger<CedulaController> _logger;
 
@@ -14,14 +23,20 @@ namespace src.Controllers
             _logger = logger;
         }
         [HttpGet(Name = "GetCedula")]
-        public string Get()
+        public string Get([FromHeader] string cedula)
         {
-            var diego = new Usuario(1724975386, "Diego");
-            var encryptedCedula = System.Text.Encoding.UTF8.GetBytes(diego.Cedula.ToString());
-            var encodingCedula = System.Convert.ToBase64String(encryptedCedula);
-            var status = Response.StatusCode;
-            _logger.LogWarning($"||METODO GET||cedula:{encodingCedula}||CODIGO {status}");
-            return $"||METODO GET||{encodingCedula}||CODIGO {status}||";
+            if (usuarios.Any(x => x.Cedula.Equals(cedula)))
+            {
+                var encryptedCedula = System.Text.Encoding.UTF8.GetBytes(cedula);
+                var encodingCedula = System.Convert.ToBase64String(encryptedCedula);
+                var status = Response.StatusCode;
+                _logger.LogWarning($"||METODO GET||cedula:{encodingCedula}||CODIGO {status}");
+                return $"||METODO GET||{encodingCedula}||CODIGO {status}||";
+            }
+            else
+            {
+                throw new ArgumentException($"No hay la {cedula} que solicita");
+            }
         }
     }
 }
--- src/Usuario.cs
+++ src/Usuario.cs
@@ -2,13 +2,9 @@ namespace src;
 
 public class Usuario
 {
-    public Usuario(long cedula, string nombre)
-    {
-        Cedula = cedula;
-        Nombre = nombre;
-    }
 
-    public long Cedula { get; set; }
+
+    public string Cedula { get; set; }
 
     public string Nombre { get; set; }
 
